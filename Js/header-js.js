bd8c644aa656f0338675d5d336a30c2991ce03b6
Mykkolla <tarabukin.n.10@gmail.com>
2022-11-15 20:35:01 +0200

    423

--- Js/header-js.js
+++ Js/header-js.js
@@ -11,3 +11,17 @@
 //     header.classList.remove('no-transparency');
 //   }
 // }
+
+(() => {
+    document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+        e.preventDefault();
+
+        new FormData(e.currentTarget).forEach((value, name) =>
+            console.log(`${name}: ${value}`),
+        );
+
+        e.currentTarget.reset();
+    });
+})();
+
+
